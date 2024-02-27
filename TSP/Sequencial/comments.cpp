0992c1dda337bd75b4e237b2d3c0dc47fa93b300
IsaacKosloski <78985850+IsaacKosloski@users.noreply.github.com>
2024-02-27 16:43:38 -0400

    Delete TSP directory

--- TSP/Sequencial/comments.cpp
+++
@@ -1,9 +0,0 @@
-/*initialSolution = new Node[tsp->dimensionOfNodes];
-cout << "dimensionOfNodes: " << tsp->dimensionOfNodes << endl;
-cout << "distanceType: " << tsp->distanceType << endl;
-cout << "explicitDistanceFormat: " << tsp->explicitDistanceFormat << endl;
-printListOfNode(tsp->nodes, tsp->dimensionOfNodes);
-printTableOfNode(tsp->nodesDistance, tsp->dimensionOfNodes);
-
-for (int i = 0; i < tsp->dimensionOfNodes; i++)
-cout << initialSolution[i].ID << " ";*/
