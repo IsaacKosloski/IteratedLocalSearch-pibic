26486c58fec9d2a59167493416475bdfa7f97ded
Isaac Koslowki <isaac@tempestade.facom.ufms.br>
2024-07-26 15:04:33 -0400

    Test: New solutions files - fl1577

--- TSP/Sequential/bin/tspSeq.sh
+++ TSP/Sequential/bin/tspSeq.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-disown -h
-./TspSeq ../../Benchmarks/pcb1173.tsp/pcb1173.tsp pcb1173
-disown -h
-./TspSeq ../../Benchmarks/d1291.tsp/d1291.tsp d1291
-disown -h
 ./TspSeq ../../Benchmarks/fl1577.tsp/fl1577.tsp fl1577
-disown -h
+
